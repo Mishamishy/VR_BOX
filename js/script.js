73d4d645cab15e92e7faa0ded4c23480c5005ad3
Mikhail <49234732+Mishamishy@users.noreply.github.com>
2019-09-19 12:04:35 +0300

    Финал

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
-$(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
+let modalBg = document.querySelector('.modal-bg'),
+	modalShow = document.querySelector('.modal-show'),
+	modalBtn = document.querySelectorAll('.button-show'),
+	modalClose = document.querySelector('.modal-close');
+	
+	modalBtn.forEach(button => {
+		button.addEventListener("click", function(){
+			modalBg.tran
+			modalBg.style.display = 'block';
+			modalShow.style.display = 'flex';
+		})
 	});
+	modalClose.onclick = () => {
+		console.log('work')
+		modalBg.style.display = 'none';
+		modalShow.style.display = 'none';
+	}
 
-});
+document.onkeydown = (event) => {
+	if (event.keyCode == 27) {
+		modalBg.style.display = 'none';
+			modalShow.style.display = 'none'; 
+	}
+}
+
+
